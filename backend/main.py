49f86471cf8e4813f8af3b227d1a7c56dea428f3
Nicolas Barrière <nicolas.barriere@student-cs.fr>
2025-07-31 18:23:08 +0200

    add customized slug

--- backend/main.py
+++ backend/main.py
@@ -24,14 +24,14 @@ slugs = ["ta-mere-en-slip", "gros-fouuu"]
 @app.post("/shorten")
 def shorten(url: str = Form(...), slug: str = Form(None)):
     if slug:
-        if slug in db:
-            raise HTTPException(status_code=400, detail="Ce slug est déjà pris.")
+        # if slug in db:
+        #     raise HTTPException(status_code=400, detail="Ce slug est déjà pris.")
         db[slug] = url
         return {"short_url": f"{BASE_URL}/r/{slug}"}
     else:
         slug_choice = random.choice(slugs)
-        while slug_choice in db:
-            slug_choice = random.choice(slugs)
+        # while slug_choice in db:
+        #     slug_choice = random.choice(slugs)
         db[slug_choice] = url
         return {"short_url": f"{BASE_URL}/r/{slug_choice}"}
 
